09b473990fa706f6635cfa4471217bab1bdaabc5
Савоськин Роман <sv-roman0502@yandex.kz>
2023-02-14 14:55:16 +0600

    add examples - hour 1

---
+++ week4/tuesday1416/list_generation.py
@@ -0,0 +1,24 @@
+def simple_generator(n):
+    idx = 0
+    while idx < n:
+        yield idx
+        idx += 1
+
+
+s = simple_generator(n=1_000_000_000)  # s - generator; next(s)
+print(s)
+# for i in simple_generator(n=5):
+#     print(i)
+#
+# s = simple_generator(5) # 0; gen -
+# next(s)  # 0
+# print(next(s))  # 1
+# print(next(s)) # 2
+# print(next(s)) # 3
+
+
+# range_numbers = range(0, 1_000_000_000)
+
+
+# list_numbers = [i for i in range(0, 1_0)]
+# print(type(range_numbers), type(list_numbers))
